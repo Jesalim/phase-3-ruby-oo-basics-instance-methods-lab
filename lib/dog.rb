4ec39812c7d3eb30dff07d5ddf41a64c791ed7e7
Jesalim <jessyculture@gmail.com>
2023-02-10 19:35:42 +0300

    instances

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+# class Dog
+# end
+
+# fido = Dog.new
+
+# fido.object_id
+# # fido.methods
+
+class Dog
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
+
+snoopy = Dog.new
+snoopy.bark
+snoopy.sit
